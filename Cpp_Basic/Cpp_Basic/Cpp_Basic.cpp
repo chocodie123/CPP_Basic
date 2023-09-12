bee0a87b0caaba41f4a31f4b5a6ff7d2be9809bd
chocodie123 <144770962+chocodie123@users.noreply.github.com>
2023-09-12 16:56:43 +0900

    littleplus

--- Cpp_Basic/Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic/Cpp_Basic.cpp
@@ -1,11 +1,11 @@
 // Cpp_Basic.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include "io.h"
 
 int main()
 {
-    std::cout << "HellO SESAC!\n";
+    cout << "HellO SESAC!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
